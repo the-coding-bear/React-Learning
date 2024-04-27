25aedefd113a945b35b72b6cdf22790710594389
the_coding_bear <akanksha060801@gmail.com>
2024-04-27 21:57:59 +0530

    new file is added

--- React/y/src/App.jsx
+++ React/y/src/App.jsx
@@ -1,8 +1,14 @@
+import Newfile from "./Newfile";
+
+
 function App() {
   
 
   return (
+    <>
     <h1>React Learning with vite</h1>
+    <Newfile/>
+    </>
   )
 }
 
---
+++ React/y/src/Newfile.jsx
@@ -0,0 +1,11 @@
+function Newfile() {
+  
+
+    return (
+      <h1>New file is added</h1>
+    )
+
+  }
+  
+  export default Newfile
+  
