c723f79ffdc4c358f417b074acdf7e689d2e414b
the_coding_bear <akanksha060801@gmail.com>
2024-04-30 17:22:52 +0530

    own react library and jsx

--- React/y/src/App.jsx
+++ React/y/src/App.jsx
@@ -2,12 +2,12 @@ import Newfile from "./Newfile";
 
 
 function App() {
-  
+  const username = "hello there"
 
   return (
     <>
-    <h1>React Learning with vite</h1>
-    <Newfile/>
+    <h1>React Learning with vite {username}</h1>
+    <p>test para</p>
     </>
   )
 }
