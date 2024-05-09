3255810426b00e64c5b30e908a906ef476867e62
the_coding_bear <akanksha060801@gmail.com>
2024-05-10 00:39:54 +0530

    changes

--- React/02counter/src/App.jsx
+++ React/02counter/src/App.jsx
@@ -6,16 +6,13 @@ import './App.css'
 function App() {
 
   let counter = 15
+
+  const addValue = () => {
+    console.log("value added", Math.random());
+  }
   return (
     <>
-      <h1>React 1</h1>
-      <h2>Counter value : {counter}</h2>
-
-      <button
-      onClick={addValue}
-      >Add value</button>
-      <br />
-      <button>remove value</button>
+     <h2>code</h2>
     </>
   )
 }
