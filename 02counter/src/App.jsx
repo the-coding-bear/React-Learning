283d18373e4ecab55db5128245c85e7662a968d1
Akanksha <akanksha060801@gmail.com>
2024-05-15 18:57:46 +0530

    initial commit of this project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Code and React</h1>
+     <h2>Counter value : 5</h2>
+
+     <button>Add value</button>
+     <br />
+     <button>remove value</button>
+    </>
+  )
+}
+
+export default App
