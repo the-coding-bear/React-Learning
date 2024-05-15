b977bf93074fb1e9c02afba09e82f4271ffd9871
Akanksha <akanksha060801@gmail.com>
2024-05-15 19:32:24 +0530

    Using hooks to add or remove values

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,15 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+ 
+ let [counter, setCounter] = useState(15)
+ 
+  //let counter = 15
+
+  const addValue = () => {
+    setCounter((prevCounter) =>{
+      if(prevCounter<20){
+        const newCounter = prevCounter +1
+        console.log('clicked' , newCounter);
+        return newCounter
+      }else{
+        console.log('Counter cannot greater than 20');
+        return prevCounter
+      }
+    });
+
+  }
+
+  const removeValue = () => {
+    setCounter((prevCounter) =>{
+      if(prevCounter>0){
+        return prevCounter - 1;
+      }else{
+        console.log('Counter cannot be negative');
+        return prevCounter
+      }
+    });
+
+  }
 
   return (
     <>
      <h1>Code and React</h1>
-     <h2>Counter value : 5</h2>
+     <h2>Counter value : {counter}</h2>
 
-     <button>Add value</button>
+     <button
+     onClick={addValue}
+     >Add value{counter}</button>
      <br />
-     <button>remove value</button>
+     <button
+     onClick={removeValue}
+     >Remove value{counter}</button>
+     <p>footer: {counter}</p>
     </>
   )
 }
