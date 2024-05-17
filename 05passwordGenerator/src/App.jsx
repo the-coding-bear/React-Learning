8f7075de6e7de80b74bce6fa20e5840b99270886
Akanksha <akanksha060801@gmail.com>
2024-05-17 22:18:58 +0530

    useEffect ,useRef are used

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState , useCallback } from 'react'
+
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed,setNumberAllowed] = useState
+  (false);
+  const [charAllowed , setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+={}[]~`"  
+
+  for (let i = 1; i <= array.length; i++){
+    let char = Math.floor(Math.random()* str.length + 1)
+    pass = str.charAt(char)
+  }
+  setPassword(pass)
+
+  }, [length,numberAllowed,charAllowed,setPassword])
+
+  return (
+    <>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gra\'>Test</div>
+    </>
+  )
+}
+
+export default App
